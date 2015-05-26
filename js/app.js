f74895bfe5e7561e3c29f3c7688a39dcf71115aa
Quentin Chouleur <qchouleur@pobox.com>
2015-05-26 23:37:20 +0200

    add multiple clock with timezone support

--- js/app.js
+++ js/app.js
@@ -1,23 +1,33 @@
 $(document).ready(function() {
 
 	function displayTime() {
-		var meridiem = "AM";
-		var currentTime = new Date();
-		var hours = currentTime.getHours();
-		var minutes = currentTime.getMinutes();
-		var seconds = currentTime.getSeconds();
 
-		if (hours > 12) {
-			hours -= 12;
-			meridiem = "PM";
-		}
-		if (!hours) {
-			hours = 12;
-		}
+		var clocks = $(".clock-screen");
 
-		var clockElement = document.getElementById('clock');
-		clockElement.innerHTML = hours.toString().paddingLeft('00')+ ":" + minutes.toString().paddingLeft('00') 
-			+ ":" + seconds.toString().paddingLeft('00') + " " + meridiem;
+		clocks.each(function() { 
+
+			var timeZone = parseInt($(this).attr('data-time-zone'), 10);
+			var date = new Date();
+			var gmtDate = new Date(date.valueOf() + date.getTimezoneOffset() * 60 * 1000);
+			gmtDate.setHours(gmtDate.getHours() + timeZone);
+
+			var meridiem = "AM";
+			var hours = gmtDate.getHours();
+			var minutes = gmtDate.getMinutes();
+			var seconds = gmtDate.getSeconds();
+			if (hours > 12) {
+				hours -= 12;
+				meridiem = "PM";
+			}
+			if (!hours) {
+				hours = 12;
+			}
+
+			$(this).html(hours.toString().paddingLeft('00')+ ":" + minutes.toString().paddingLeft('00') 
+					+ ":" + seconds.toString().paddingLeft('00') + " " + meridiem);
+
+		});
+
 	};
 
 	displayTime();
